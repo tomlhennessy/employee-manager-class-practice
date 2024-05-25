5fc169a45df711c4546a9bf4d9859a61968600aa
Tom Hennessy <tomlhennessy@outlook.com>
2024-05-25 15:04:26 +0100

    stage 2: manager class complete

---
+++ manager.js
@@ -0,0 +1,30 @@
+const Employee = require('./employee');
+
+class Manager extends Employee {
+    constructor(name, salary, title, manager = null) {
+        super(name, salary, title, manager);
+        this.employees = [];
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+}
+
+module.exports = Manager;
+
+// Local testing
+const splinter = new Manager('Splinter', 100000, 'Sensei');
+console.log('Before: ', splinter);
+
+const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+splinter.addEmployee(leo);
+splinter.addEmployee(mikey);
+splinter.addEmployee(donnie);
+splinter.addEmployee(raph);
+
+console.log('After: ', splinter);
