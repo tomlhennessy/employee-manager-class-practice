48f324dff3b673b0cfc498ada7ee2cea02323822
Tom Hennessy <tomlhennessy@outlook.com>
2024-05-25 18:41:01 +0100

    added calculateBonus method

--- employee.js
+++ employee.js
@@ -28,10 +28,17 @@ class Employee {
 
         people.forEach((person) => person.introduce());
     }
+
+    calculateBonus(multiplier) {
+        return this.salary * multiplier;
+    }
 }
 
 module.exports = Employee;
 
-/* Local testing
-const leo = new Employee('leonardo', 90000, 'Ninja');
-console.log(leo); */
+// Local testing
+/* const raph = new Employee('Raphael', 90000, 'Ninja');
+const donny = new Employee('Donatello', 85000, 'Grasshopper');
+
+console.log(raph.calculateBonus(0.25)); // => 22500
+console.log(donny.calculateBonus(0.15)); // => 12750 */
--- manager.js
+++ manager.js
@@ -9,20 +9,34 @@ class Manager extends Employee {
     addEmployee(employee) {
         this.employees.push(employee);
     }
+
+    calculateBonus(multiplier) {
+        const totalSalary = this.salary + this._totalSubSalary();
+        return totalSalary * multiplier;
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for (const employee of this.employees) {
+            sum += employee.salary;
+            if (employee instanceof Manager) {
+                sum += employee._totalSubSalary();
+            }
+        }
+        return sum;
+    }
 }
 
 module.exports = Manager;
 
 // Local testing
-const Manager = require('./manager');
-const Employee = require('/.employee');
-
 const splinter = new Manager('Splinter', 100000, 'Sensei');
-console.log('Before: ', splinter);
+const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+const mikey = new Employee('Michelangelo', 85000, 'Ninja', raph);
+const donnie = new Employee('Donatello', 85000, 'Ninja', raph);
 
-const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
-const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
-const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
-const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
 
-console.log('After: ', splinter);
+console.log(splinter.calculateBonus(0.05)); // => 22500
+console.log(leo.calculateBonus(0.05)); // => 17500
+console.log(raph.calculateBonus(0.05)); // => 13000
